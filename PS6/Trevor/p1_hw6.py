3df480357800b7eaddde8516939490d86bd583b2
Heejune Park <heepark@ucdavis.edu>
2022-12-07 11:41:35 -0800

    feedback for PS6

--- PS6/Trevor/p1_hw6.py
+++ PS6/Trevor/p1_hw6.py
@@ -63,6 +63,9 @@ species = ['H2', 'N2', 'NH3']
 mol_mass = [2.016, 28.02, 17.03]
 '''
 Why are we able to use 6 instead of 3?
+HP: Three of them, from C_3, are obvious. The other three are tricky. 
+    You can also rotate NH3 with an axis that is perpendicular to the C_3 axis.
+    I think that's why rotational sym. number is 6. 
 '''
 sym_number = [2, 2, 6]
 rot_const = [60.85300, 1.99824, [9.44430, 9.44430, 6.19600]]
@@ -77,6 +80,8 @@ for count, e in enumerate(energy):
 It would be nice to make a nested dictionary, so that you could intuitively extract info
 ex) species_data_dict['H2']['sym_number']
 
+HP: You are right. In fact there is something called json that I use quite often. It's like dictionaries inside of a dictionary.
+
 It requires more steps when extracting info for making table.
 Need to practice with nested dictionaries
 '''
@@ -88,7 +93,7 @@ species_data_dict['vib_freq \n cm^-1'] = vib_freq
 species_data_dict['rel_energy \n kJ/molecule'] = rel_e
 
 print(species_data_dict.keys())
-
+# HP: Cool table
 print(tabulate(species_data_dict, headers='keys', tablefmt='fancy_grid'))
 
 '''
@@ -122,14 +127,17 @@ def translation_partition_function(mass, temp):
 tr = translation_partition_function(species_data_dict['mol_mas \namu'][1], 273.15)
 print(f"compare this to nitrogen Ztr {tr*3.720e-26}")
 
+# HP: I modified the following two functions for the Rotational partition function 
+
 def rotational_partition_highT_approx_linear(sigma, temp, rot_const):
     h = 6.62607004e-34 # j * s
     kb = 1.380649 * 10 ** -23 # J/K
-    freq = c * rot_const * 100 #Hz
-    E = freq * h
-    rot_temp = E/ kb
-    T = temp
-    z = T / (sigma * rot_temp)
+    freq = c * rot_const * 100 * h #Hz
+    #E = freq * h
+    #rot_temp = E/ kb
+    #T = temp
+    #z = T / (sigma * rot_temp)
+    z = kb*temp/(sigma*freq)
     return z
 print(f" test rotation part func {rotational_partition_highT_approx_linear(2, 273.15, 1.99824)}")
 
@@ -140,16 +148,16 @@ def rotational_partition_highT_approx_nonlinear(sigma, temp, rot_constant):
 
     h = 6.62607004e-34 # j * s
     kb = 1.380649 * 10 ** -23 # kJ/mol
-    freq1 = c * A * 100 #Hz
-    freq2 = c * B * 100 #Hz
-    freq3 = c * C * 100  # Hz
+    freq1 = c * A * 100 * h#Hz
+    freq2 = c * B * 100 * h#Hz
+    freq3 = c * C * 100  * h# Hz
    #E1 = freq1 * h/1000 * NA # kJ/mol
     #E2 = freq2 * h/1000 * NA # kJ/mol
     #E3 = freq3 * h/1000 * NA # kJ/mol
-    E1 = freq1 * h/1000  # kJ/mol
-    E2 = freq2 * h/1000   # kJ/mol
-    E3 = freq3 * h/1000   # kJ/mol
-    z = (np.sqrt(np.pi)/sigma) * np.sqrt(((kb*temp)**3) / (E1 * E2 * E3))
+    #E1 = freq1 * h/1000  # kJ/mol
+    #E2 = freq2 * h/1000   # kJ/mol
+    #E3 = freq3 * h/1000   # kJ/mol
+    z = (np.sqrt(np.pi)/sigma) * np.sqrt(((kb*temp)**3) / (freq1 * freq2 * freq3))
     return z#, E1, E2, E3
 #print(f" rot nonllinear{rotational_partition_highT_approx_nonlinear(2, 273.15, [27.877, 14.512, 9.285])}")
 
@@ -205,39 +213,76 @@ def haber_equil_const_empirical_fit(temp):
     log_Keq = 2.1 + (2098/temp) - (2.5088 * np.log10(temp)) - (1.006e-4 * temp) + (1.86e-7 * temp**2) + np.log10(0.0821 * temp)
     return log_Keq
 
-temps = np.arange(800, 1200, 10)
-print(10**haber_equil_const_empirical_fit(800))
-
-plt.plot(temps, haber_equil_const_empirical_fit(temps))
-plt.show()
-
+temps = np.arange(800, 1200, 50)
 
 conc_std = 6.022e26
 volume = 3.720e-26
-Keq = []
-for temp in temps:
-    z_trans_H2 = translation_partition_function(species_data_dict['mol_mas \namu'][0], temps)
-    z_vib_H2 = vib_partition_function(temps, [species_data_dict['vib_freq \n cm^-1'][0]])
-    z_el_H2 = electronic_partition_function(temps, species_data_dict['rel_energy \n kJ/molecule'][0])
-    z_rot_H2 = rotational_partition_highT_approx_linear(2, temps, species_data_dict['rot_const \n cm^-1'][0])
-    z_H2 = z_el_H2 * z_rot_H2 * z_vib_H2 * z_trans_H2
+#Keq = []
 
-    z_trans_N2 = translation_partition_function(species_data_dict['mol_mas \namu'][1], temps)
-    z_vib_N2 = vib_partition_function(temps, [species_data_dict['vib_freq \n cm^-1'][1]])
-    z_el_N2 = electronic_partition_function(temps, species_data_dict['rel_energy \n kJ/molecule'][1])
-    z_rot_N2 = rotational_partition_highT_approx_linear(2, temps, species_data_dict['rot_const \n cm^-1'][1])
-    z_N2 = z_vib_N2 * z_rot_N2 * z_el_N2  * z_trans_N2
+# HP: Added this funciton to debug
+def Keq (H, N, NH, T):
+    """
+    This function calculates Keq of Haber-Bosch reaction with given partition functions.
+    
+    Parameters
+    ----------
+    T : array
+        An array of temperatrues
+    
+    H : float
+        Partition function of H2
+    N : float
+        Partition function of N2
+    NH : float
+        Partition function of NH3
+    Return
+    ------
+    keq : array
+        An array of Keq.
+    """
+    H2 = H**(3/2)
+    N2 = N**(1/2)
+    keq = NH/(H2*N2)
+    return keq
 
-    z_trans_NH3 = translation_partition_function(species_data_dict['mol_mas \namu'][2], temps)
-    z_vib_NH3 = vib_partition_function(temps, species_data_dict['vib_freq \n cm^-1'][2])
-    z_el_NH3 = electronic_partition_function(temps, species_data_dict['rel_energy \n kJ/molecule'][2])
-    z_rot_NH3 = rotational_partition_highT_approx_nonlinear(6, temps, species_data_dict['rot_const \n cm^-1'][2])
-    z_NH3 = z_el_NH3 * z_rot_NH3 * z_vib_NH3 * z_trans_NH3
+# HP: You don't need to loop through the array. You can just feed it as an array, then you will get the results as an array. 
+#     It's called vectorization.
 
+#for temp in temps:
+z_trans_H2 = translation_partition_function(species_data_dict['mol_mas \namu'][0], temps) 
+z_vib_H2 = vib_partition_function(temps, [species_data_dict['vib_freq \n cm^-1'][0]])
+z_el_H2 = electronic_partition_function(temps, species_data_dict['rel_energy \n kJ/molecule'][0])
+z_rot_H2 = rotational_partition_highT_approx_linear(2, temps, species_data_dict['rot_const \n cm^-1'][0])
+#z_H2 = z_el_H2 * z_rot_H2 * z_vib_H2 * z_trans_H2
 
-    Keq.append(volume * conc_std * z_NH3 / (((np.sqrt(z_N2)) * (np.sqrt(z_H2))**3)))
+z_trans_N2 = translation_partition_function(species_data_dict['mol_mas \namu'][1], temps)
+z_vib_N2 = vib_partition_function(temps, [species_data_dict['vib_freq \n cm^-1'][1]])
+z_el_N2 = electronic_partition_function(temps, species_data_dict['rel_energy \n kJ/molecule'][1])
+z_rot_N2 = rotational_partition_highT_approx_linear(2, temps, species_data_dict['rot_const \n cm^-1'][1])
+#z_N2 = z_vib_N2 * z_rot_N2 * z_el_N2  * z_trans_N2
 
-plt.plot(temps, 10**haber_equil_const_empirical_fit(temps))
+z_trans_NH3 = translation_partition_function(species_data_dict['mol_mas \namu'][2], temps)
+z_vib_NH3 = vib_partition_function(temps, species_data_dict['vib_freq \n cm^-1'][2])
+z_el_NH3 = electronic_partition_function(temps, species_data_dict['rel_energy \n kJ/molecule'][2])
+z_rot_NH3 = rotational_partition_highT_approx_nonlinear(6, temps, species_data_dict['rot_const \n cm^-1'][2])
+#z_NH3 = z_el_NH3 * z_rot_NH3 * z_vib_NH3 * z_trans_NH3
+
+Keq_tr = Keq(z_trans_H2, z_trans_N2, z_trans_NH3, temps) * conc_std
+Keq_rot = Keq(z_rot_H2, z_rot_N2, z_rot_NH3, temps)
+Keq_vib = Keq(z_vib_H2, z_vib_N2, z_vib_NH3, temps)
+Keq_el = Keq(z_el_H2, z_el_N2, z_el_NH3, temps)
+
+print(Keq_tr)
+print(Keq_rot)
+print(Keq_vib)
+print(Keq_el)
+
+Keq = Keq_tr * Keq_rot * Keq_vib * Keq_el
+#Keq = volume * conc_std * z_NH3 / (((np.sqrt(z_N2)) * (np.sqrt(z_H2))**3))
+#Keq.append()
+plt.plot(temps, np.log10(Keq), label='From Partition Functions')
+plt.plot(temps, haber_equil_const_empirical_fit(temps), label ='Empirical')
+plt.legend()
 plt.show()
 '''
 e)
